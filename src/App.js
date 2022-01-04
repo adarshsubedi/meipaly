d165f66a09be7acd23285fc850c8d4393f748094
unknown <adarsh9860@gmail.com>
2022-01-04 16:32:43 +0545

    ksdb

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/*import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar/navbar';
+
+function App() {
+  
+  
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+
+    );
+}
+
+export default App;
+
+
+*/
---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,16 @@
+/*import React, {Components} from "react";
+import './Navbar'
+
+class Navbar extends Components () {
+    render() {
+        return(
+            <nav className="">
+
+            </nav>
+        )
+
+    }
+}
+
+export default Navbar
+*/
